5502534977af432fc180746608710ae59edac2ad
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2015-05-27 00:23:40 +0200

    typo
    Change-Id: If894b99f9663b0fe3f2094899171122f03dd3412

--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,5 @@
 def FullOTA_InstallEnd(info):
 	info.script.AppendExtra('symlink("/system/lib/libjhead.so", "/system/lib/libhead.so");')
 	info.script.SetPermissionsRecursive("/system/UKM", 0, 0, 0755, 0755, None, None)
-	info.script.MakeSymlinks((("/data/UMK/uci", "/system/xbin/uci"), ("/system/UMK/UMK","/system/etc/init.d/99UMK")))
+	info.script.MakeSymlinks((("/data/UKM/uci", "/system/xbin/uci"), ("/system/UKM/UKM","/system/etc/init.d/99UKM")))
 	#info.script.AppendExtra('run_program("/sbin/make_ext4fs", "/dev/block/mmcblk0p9");')
