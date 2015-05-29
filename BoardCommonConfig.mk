f99de4defa2243b3c636a02435f67e990a101840
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2015-05-29 21:13:31 +0200

    cpu cache info for gcc
    commented out because clang complains about it (needs -Qunused-argument)
    
    Change-Id: I689d505a7dc2f028465b29d337f7aa6a517e2ede

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -48,6 +48,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 ARCH_ARM_HAVE_NEON := true
 TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+#TARGET_GLOBAL_CFLAGS += --param l1-cache-size=32 --param l1-cache-line-size=32 --param l2-cache-size=512
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 
 # Kernel
