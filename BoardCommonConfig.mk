a7247d0530a997e2246ee71dab67de915df86da3
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2015-05-31 14:30:02 +0200

    it does build with 4.9 linaro gcc :)
    Change-Id: I0c04cc2b4c3cdfd2acf1c5e340d12c564f8bfe4a

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -56,8 +56,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_BASE := 0x40000000
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/u8500-common/shbootimg.mk
 TARGET_KERNEL_SOURCE := kernel/samsung/u8500
-#doesn't boot with 4.9
-TARGET_GCC_VERSION=4.8-linaro
+TARGET_GCC_VERSION=4.9-linaro
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := 4.9
 
 # Graphics
