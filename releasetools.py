a6a6ee5873bd141821cee050c5b13b1aef9ccb48
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2015-06-02 18:15:13 +0200

    remove UKM symlinks
    Change-Id: Ib6d0c042ee6775bf11cdf3ca3894fe004b04fc17

--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,3 @@
 def FullOTA_InstallEnd(info):
 	info.script.AppendExtra('symlink("/system/lib/libjhead.so", "/system/lib/libhead.so");')
-	info.script.SetPermissionsRecursive("/system/UKM", 0, 0, 0755, 0755, None, None)
-	info.script.MakeSymlinks((("/data/UKM/uci", "/system/xbin/uci"), ("/system/UKM/UKM","/system/etc/init.d/99UKM")))
 	#info.script.AppendExtra('run_program("/sbin/make_ext4fs", "/dev/block/mmcblk0p9");')
